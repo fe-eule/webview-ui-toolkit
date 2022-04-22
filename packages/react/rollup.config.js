d94934baa0c7985d2cfe80790ac4aa4e8275e7ea
Sungdong Jo <sdong001@gmail.com>
2022-04-23 06:23:51 +0900

    Modify rollup

--- packages/react/rollup.config.js
+++ packages/react/rollup.config.js
@@ -10,7 +10,6 @@ module.exports = defaultConfig => {
   };
   const esmOutputConfig = {
     preserveModules: true,
-    preserveModulesRoot: 'packages/ts-utils/src',
   };
   const formatOutputConfig = isEsm ? esmOutputConfig : {};
 
